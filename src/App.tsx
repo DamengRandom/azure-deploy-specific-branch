5cf2e4838330114bd25386fe3412d3bf5b768594
Damon Wu <damonmaozewu@gmail.com>
2021-07-31 16:59:51 +1000

    update home page content

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Target branch to deploy ..
+          Target branch to deploy to azure service provider ..
         </p>
       </header>
     </div>
